14007db39a244145dd329ba9b4406e8d211e190b
Mohsin Ali Khan <mohsinalikhan9260@gmail.com>
2026-08-04 15:31:43 +0500

    Fix missing Header and Footer on home page
    HomeTemplate was missing Header/Footer wrappers causing invisible footer and broken layout

--- src/components/HomeTemplate.tsx
+++ src/components/HomeTemplate.tsx
@@ -1,19 +1,25 @@
+import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import BreakingNews from "@/components/BreakingNews";
 import VideoNews from "@/components/VideoNews";
 import TopOfWeek from "@/components/TopOfWeek";
 import TopStories from "@/components/TopStories";
 import Subscribe from "@/components/Subscribe";
+import Footer from "@/components/Footer";
 
 export default function HomeTemplate() {
   return (
-    <>
-      <Hero />
-      <BreakingNews />
-      <VideoNews />
-      <TopOfWeek />
-      <TopStories />
-      <Subscribe />
-    </>
+    <div className="nerio-page-wrapper flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1">
+        <Hero />
+        <BreakingNews />
+        <VideoNews />
+        <TopOfWeek />
+        <TopStories />
+        <Subscribe />
+      </main>
+      <Footer />
+    </div>
   );
 }
